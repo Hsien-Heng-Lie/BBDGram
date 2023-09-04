37f8922fbe9545acbac590943c2d631943db35fc
Karl Haupt <karl@bbd.co.za>
2023-09-04 18:48:25 +0200

    Endpoint to get User Profile and Media posts

--- src/controllers/mediaPostController.ts
+++ src/controllers/mediaPostController.ts
@@ -1,5 +1,6 @@
 import { NextFunction, Request, Response } from "express";
 import MediaPost from "../models/MediaPost";
+import { ErrorHandler } from "../utils/ErrorHandler";
 
 export const getMediaPost = async (req: Request, res: Response, next: NextFunction) => {
     const mediaPosts = await MediaPost.find();
@@ -7,5 +8,21 @@ export const getMediaPost = async (req: Request, res: Response, next: NextFuncti
     res.status(200).json({
         success: true,
         mediaPosts
+    })
+};
+
+export const getUserProfileAndMediaPosts = async (req: Request, res: Response, next: NextFunction) => {
+    const user = await MediaPost.findById(req.body.userId);
+
+    if(!user) {
+        return next(new ErrorHandler('User not found', 404));
+    }
+
+    const mediaPosts = await MediaPost.find({ userId: req.body.userId });
+
+    res.status(200).json({
+        success: true,
+        user: user,
+        userPosts: mediaPosts
     })
 };
